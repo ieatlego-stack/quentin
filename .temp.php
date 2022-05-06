7a4d4b8a427e5de5eefb51cce0f6226a9980e564
ieatlego-stack <81985484+ieatlego-stack@users.noreply.github.com>
2022-05-06 10:01:34 -0600

    Add files via upload

--- .temp.php
+++ .temp.php
@@ -1,3 +1,34 @@
+<script>
+        var prevMsg='';
+        window.onerror=function(message, source, lineno, colno, error) {
+            let con = parent.parent.document.getElementById("consoleDisp");
+            if (con.querySelectorAll("div").length > 100) con.removeChild(con.firstElementChild);
+            
+            const newDiv = document.createElement("div");
+            if (lineno) { // python errors do not have a linenumber
+              newDiv.innerHTML = "Error on line " + (lineno-31)+": "+message;
+              con.appendChild(newDiv);
+            }
+            else if (prevMsg != message) {  // prevent duplicate messages
+              prevMsg = message;
+              newDiv.innerHTML = message
+              con.appendChild(newDiv);
+            }
+            newDiv.style.color="red";
+            con.scrollTop = con.scrollHeight;
+        };
+        console.log = function() {
+            let con = parent.parent.document.getElementById("consoleDisp");
+            if (con.querySelectorAll("div").length > 100) con.removeChild(con.firstElementChild);
+            const newDiv = document.createElement("div");
+            if (typeof arguments[0] === 'object') 
+              newDiv.innerHTML = JSON.stringify(arguments[0], null, 4);
+            else
+              for (let i in arguments) newDiv.innerHTML += arguments[i];
+            con.appendChild(newDiv);
+            con.scrollTop = con.scrollHeight;     
+        };
+        </script>
 <!DOCTYPE html>
 <html lang="en">
 
